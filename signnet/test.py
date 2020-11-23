35bae987d2a4f807534df916a4a94f1db072dc25
Zeqiang-Lai <916323301@qq.com>
2020-11-23 22:02:34 +0800

    add code to plot confusion matrix

--- signnet/test.py
+++ signnet/test.py
@@ -4,27 +4,54 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import numpy as  np
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from options.test_options import TestOptions
-from dlcommon.json import save_dict
+from dlcommon.json import load_dict
 from model.vgg import vgg11
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, classes, log_dir):
     model.eval()
     test_loss = 0
     correct = 0
+
+    pred_total = []
+    target_total = []
     with torch.no_grad():
         batch_count = 0
         for (data, target) in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
+
             batch_count += 1
             test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+
+            # confusion
+            pred_total.append(pred.detach().cpu().numpy())
+            target_total.append(target.detach().cpu().numpy())
+
+            # accuracy
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
+    pred_total = np.concatenate(pred_total, axis=0).squeeze()
+    target_total = np.concatenate(target_total, axis=0)
+    print(pred_total.shape)
+    print(target_total.shape)
+
+    confusion = confusion_matrix(target_total, pred_total)
+    df_cm = pd.DataFrame(confusion, classes, classes)
+    print(classes)
+    # sn.set(font_scale=0.4)
+    # sn.heatmap(df_cm, annot=True)
+    sn.heatmap(df_cm, annot=False, xticklabels=classes, yticklabels=classes)
+    plt.savefig(os.path.join(log_dir, 'confusion.pdf'))
 
     print('\nTest set: Average loss: {:.8}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
@@ -56,10 +83,6 @@ def get_dataloader(use_cuda, args):
     return test_loader
 
 
-def save_label_dict(path, dataset):
-    save_dict({'classes': dataset.classes, 'class2idx': dataset.class_to_idx}, path)
-
-
 def main():
     args = TestOptions().parse()
 
@@ -73,8 +96,8 @@ def main():
     log_dir = os.path.join(args.checkpoints_dir, args.resume_name)
     checkpoints = torch.load(os.path.join(log_dir, 'checkpoint_latest.pth'))
     model.load_state_dict(checkpoints['model_state_dict'])
-
-    test(model, device, test_loader)
+    mapping = load_dict(os.path.join(log_dir, 'class_map.json'))
+    test(model, device, test_loader, mapping['classes'], log_dir)
 
 
 if __name__ == '__main__':
