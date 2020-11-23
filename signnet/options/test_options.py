97714b9c7b0692dd1d094bc427cd2c4d6426f37c
Zeqiang-Lai <916323301@qq.com>
2020-11-23 21:24:10 +0800

    finish test code

--- signnet/options/test_options.py
+++ signnet/options/test_options.py
@@ -9,8 +9,12 @@ class TestOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
+        parser.add_argument('--seed', type=int, default=1, metavar='S',
+                            help='random seed (default: 1)')
+        parser.add_argument('--val-set', type=str, default='asl-alphabet-test')
+        parser.add_argument('--no-cuda', action='store_true', default=False,
+                            help='disables CUDA training')
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
-        parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
--- signnet/test.py
+++ signnet/test.py
@@ -1,66 +1,44 @@
-from __future__ import print_function
-
-import argparse
 import os
 
 import torch
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import torchvision
 
-from model import vgg11
+from options.test_options import TestOptions
+from dlcommon.json import save_dict
+from model.vgg import vgg11
 
-
-# 需要去掉测试集中几张200*150的图片,否则会报错,这些图片位于C这个类别中.
 
 def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        batch_count = 0
+        for (data, target) in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
+            batch_count += 1
             test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-            # data: B, 1, 64, 64
-            cools = []
-            for idx, i in enumerate(pred.eq(target.view_as(pred))):
-                if i.item():
-                    cools.append(data[idx])
-            image = torchvision.utils.make_grid(cools)
-            torchvision.utils.save_image(image, 'cool.jpg')
-
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    print('\nTest set: Average loss: {:.8}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--data-path', type=str, default='./data',
-                        help='Where the asl alphabet dataset is')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    test_kwargs = {'batch_size': args.test_batch_size}
-
+def get_dataloader(use_cuda, args):
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.batch_size}
     if use_cuda:
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
                        'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
     transform = transforms.Compose([
@@ -69,14 +47,35 @@ if __name__ == '__main__':
         transforms.Resize((64, 64)),
         transforms.ToTensor(),
     ])
-    test_dataset_path = os.path.join(args.data_path, 'asl-alphabet-test')
+    test_dataset_path = os.path.join(args.data_path, args.val_set)
 
     dataset2 = datasets.ImageFolder(root=test_dataset_path, transform=transform)
 
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = vgg11().to(device)
+    return test_loader
 
-    model.load_state_dict(torch.load('./gesture_cnn_t2.pt', map_location=device))
+
+def save_label_dict(path, dataset):
+    save_dict({'classes': dataset.classes, 'class2idx': dataset.class_to_idx}, path)
+
+
+def main():
+    args = TestOptions().parse()
+
+    torch.manual_seed(args.seed)
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    test_loader = get_dataloader(use_cuda, args)
+    model = vgg11(len(test_loader.dataset.classes)).to(device)
+
+    log_dir = os.path.join(args.checkpoints_dir, args.resume_name)
+    checkpoints = torch.load(os.path.join(log_dir, 'checkpoint_latest.pth'))
+    model.load_state_dict(checkpoints['model_state_dict'])
 
     test(model, device, test_loader)
+
+
+if __name__ == '__main__':
+    main()
